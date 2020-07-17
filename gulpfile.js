7335785ff590448016bf5bae3e3a4cbcb8252ba9
Molochyk <slapibs18@gmail.com>
2020-07-17 17:58:09 +0300

    add new gulp tust (js)

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,37 @@ const	cache 				= require('gulp-cache');
 const	autoprefixer 	= require('gulp-autoprefixer');
 const	notify				= require("gulp-notify");
 const	gcmq 					= require('gulp-group-css-media-queries');
-const njkRender = require('gulp-nunjucks-render');
-// const uglify 				= require('gulp-uglify'),
+const njkRender     = require('gulp-nunjucks-render');
+const babel					= require("gulp-babel");
+const uglify 				= require('gulp-uglify');
+const del						= require('del');
 
 // Скрипты проекта
+
 gulp.task('main-js', function() {
 	return gulp.src([
-		'app/js/main.js',
-		])
-	.pipe(concat('main.js'))
-	//.pipe(uglify()) //Минимизировать js
+		'app/js/bundle.js',
+	])
+	.pipe(concat('bundle.min.js'))
+	.pipe(babel())
+	.pipe(uglify())
 	.pipe(gulp.dest('app/js'));
 });
 
-gulp.task('js', ['main-js'], function() {
+gulp.task('vendors-js', function() {
 	return gulp.src([
 		'./node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
-		'./node_modules/owl.carousel/dist/owl.carousel.min.js',
-		// 'app/libs/modernizr-webp/modernizr-webp.js',
-		// 'app/libs/aos/aos.js',
-		// 'app/libs/lazyload/lazyload.min.js',
-		'app/js/main.js', // Всегда в конце
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Минимизировать весь js (на выбор)
-	.pipe(gulp.dest('app/js'))
+		'./node_modules/swiper/js/swiper.min.js',
+		'./node_modules/imask/dist/imask.min.js',
+		'./node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
+		'./node_modules/aos/dist/aos.js',
+	])
+	.pipe(concat('vendors.min.js'))
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('js', ['main-js', 'vendors-js'], function() {
+	return gulp.src('app/js/**/*.js')
 	.pipe(browserSync.reload({stream: true}));
 });
 
@@ -74,9 +80,37 @@ gulp.task('watch', [ 'nunjucks', 'sass', 'js', 'browser-sync'], function() {
 	setTimeout(function() {
 		gulp.watch('app/sass/**/*.scss', ['sass']);
 	}, 1000);
-	gulp.watch(['libs/**/*.js', 'app/js/main.js'], ['js']);
+	gulp.watch(['app/js/bundle.js'], ['js']);
 	gulp.watch(['./app/pages' + '/**/*.+(html|nunjucks)', './app/templates' + '/**/*.+(html|nunjucks)'], ['nunjucks']);
 	// gulp.watch('./app/pages/**/*.+(html|nunjucks)', ['nunjucks'], browserSync.reload);
+});
+
+gulp.task('copy', function() {
+	gulp.src('./app/*.html')
+	.pipe(gulp.dest('dist'));
+
+	gulp.src('./app/css/*.css')
+	.pipe(gulp.dest('dist/css'));
+
+	gulp.src('./app/js/*.min.js')
+	.pipe(gulp.dest('dist/js'));
+
+	gulp.src('./app/img/**/*.{gif,jpg,png,svg,webp}')
+	.pipe(gulp.dest('dist/img'));
+
+	gulp.src('./app/fonts/**/*.{woff,woff2,ttf,eot}')
+	.pipe(gulp.dest('dist/fonts'));
+
+	gulp.src('./app/*.php')
+	.pipe(gulp.dest('dist'));
+
+	return gulp.src('./app/ht.access')
+	.pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clean', function(){
+	return del('dist/**', {force:true});
 });
 
 gulp.task('clearcache', function () { return cache.clearAll(); });
